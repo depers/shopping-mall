6fb2578f1bd783b1a153727c0307ee8b171f1c79
depers <dev_fengxiao@foxmail.com>
2017-06-21 12:23:59 +0800

    project init commit

---
+++ src/main/java/com/fmall/dao/ShippingMapper.java
@@ -0,0 +1,17 @@
+package com.fmall.dao;
+
+import com.fmall.pojo.Shipping;
+
+public interface ShippingMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+}
