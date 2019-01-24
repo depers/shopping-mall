c1ae5033c52c4f767b518cbd5a479956de810b5d
depers <dev_fengxiao@163.com>
2019-01-24 21:28:16 +0800

    commit sessionExpireTime

---
+++ src/main/java/com/fmall/controller/common/SessionExpireFilter.java
@@ -0,0 +1,46 @@
+package com.fmall.controller.common;
+
+import com.fmall.common.Const;
+import com.fmall.pojo.User;
+import com.fmall.util.CookieUtil;
+import com.fmall.util.JsonUtil;
+import com.fmall.util.RedisPoolUtil;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Created by 冯晓 on 2019/1/24.
+ */
+
+public class SessionExpireFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+
+        if(StringUtils.isNotEmpty(loginToken)){
+
+            String userJsonStr = RedisPoolUtil.get(loginToken);
+            User user = JsonUtil.string2Obj(userJsonStr, User.class);
+            if (user != null){
+                // 如果user不为空，则重置session的时间，即调用expire命令
+                RedisPoolUtil.expire(loginToken, Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
+            }
+        }
+        chain.doFilter(httpServletRequest, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
