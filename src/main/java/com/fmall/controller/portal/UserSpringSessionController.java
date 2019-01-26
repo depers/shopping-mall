9146951fb7d3e0c1de836e4192d05c5a0b0caf09
depers <dev_fengxiao@163.com>
2019-01-26 15:46:03 +0800

    spring session

---
+++ src/main/java/com/fmall/controller/portal/UserSpringSessionController.java
@@ -0,0 +1,84 @@
+package com.fmall.controller.portal;
+
+import com.fmall.common.Const;
+import com.fmall.common.ResponseCode;
+import com.fmall.common.ServerResponse;
+import com.fmall.pojo.User;
+import com.fmall.service.IUserService;
+import com.fmall.util.CookieUtil;
+import com.fmall.util.JsonUtil;
+import com.fmall.util.RedisShardedPoolUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by 冯晓 on 2017/6/21.
+ */
+@Controller
+@RequestMapping("/user/springsession/")
+public class UserSpringSessionController {
+
+    @Autowired
+    private IUserService iUserService;
+
+
+    /**
+     * 用户登录
+     * @param username
+     * @param password
+     * @param httpSession
+     * @return
+     */
+    @RequestMapping(value = "login.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<User> login(String username, String password, HttpSession httpSession, HttpServletResponse httpServletResponse){
+        ServerResponse<User> response = iUserService.login(username, password);
+        if(response.isSuccess()){
+
+            httpSession.setAttribute(Const.CURRENT_USER, response.getData());
+            //CookieUtil.writeLoginToken(httpServletResponse, httpSession.getId());
+            //RedisShardedPoolUtil.setEx(httpSession.getId(), JsonUtil.obj2String(response.getData()), Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
+
+        }
+        return response;
+    }
+
+    @RequestMapping(value = "logout.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> logout(HttpSession session, HttpServletRequest request, HttpServletResponse response){
+        session.removeAttribute(Const.CURRENT_USER);
+        //String loginToken = CookieUtil.readLoginToken(request);
+        //CookieUtil.delLoginToken(request, response);
+        //RedisShardedPoolUtil.del(loginToken);
+
+        return ServerResponse.createBySuccess();
+    }
+
+    @RequestMapping(value = "getUserInfo.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<User> getUserInfo(HttpSession session, HttpServletRequest request){
+//        String loginToken = CookieUtil.readLoginToken(request);
+//        if (StringUtils.isEmpty(loginToken)){
+//            return ServerResponse.createByErrorMessage("用户未登录，无法获取当前用户的信息。");
+//        }
+//
+//        String userJsonStr = RedisShardedPoolUtil.get(loginToken);
+//        User user = JsonUtil.string2Obj(userJsonStr, User.class);
+
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if(user != null){
+            return ServerResponse.createBySuccess(user);
+        }
+
+        return ServerResponse.createByErrorMessage("用户未登录，无法获取当前用户的信息。");
+    }
+
+}
