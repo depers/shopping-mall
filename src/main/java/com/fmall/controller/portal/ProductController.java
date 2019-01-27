c8f143ca211913dff5e69f3714157f3e5b6df0bf
depers <dev_fengxiao@163.com>
2019-01-27 20:03:44 +0800

    commit RESTful

--- src/main/java/com/fmall/controller/portal/ProductController.java
+++ src/main/java/com/fmall/controller/portal/ProductController.java
@@ -4,11 +4,10 @@ import com.fmall.common.ServerResponse;
 import com.fmall.service.IProductService;
 import com.fmall.vo.ProductDetailVo;
 import com.github.pagehelper.PageInfo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by 冯晓 on 2017/6/28.
@@ -27,6 +26,12 @@ public class ProductController {
         return iProductService.getProductDetail(productId);
     }
 
+    @RequestMapping(value = "/{productId}", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<ProductDetailVo> detailRESTful(@PathVariable Integer productId){
+        return iProductService.getProductDetail(productId);
+    }
+
 
     @RequestMapping("list.do")
     @ResponseBody
@@ -38,6 +43,105 @@ public class ProductController {
         return iProductService.getProductByKeywordCategory(keyword,categoryId, pageNum, pageSize, orderBy);
     }
 
+    //http://www.happymall.com/product/手机/100012/1/10/price_asc
+    @RequestMapping("/{keyword}/{categoryId}/{pageNum}/{pageSize}/{orderBy}")
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful(@PathVariable(value = "keyword")String keyword,
+                                         @PathVariable(value = "categoryId")Integer categoryId,
+                                         @PathVariable(value = "pageNum")Integer pageNum,
+                                         @PathVariable(value = "pageSize")Integer pageSize,
+                                         @PathVariable(value = "orderBy")String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if (pageSize == null){
+            pageNum = 10;
+        }
+        if (StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
 
+        return iProductService.getProductByKeywordCategory(keyword,categoryId, pageNum, pageSize, orderBy);
+    }
+
+    // Ambiguous handler methods mapped for HTTP path 'http://www.happymall.com/product/100012/1/10/price_asc'
+    @RequestMapping("/{categoryId}/{pageNum}/{pageSize}/{orderBy}")
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTfulBadcase(@PathVariable(value = "categoryId")Integer categoryId,
+                                                @PathVariable(value = "pageNum")Integer pageNum,
+                                                @PathVariable(value = "pageSize")Integer pageSize,
+                                                @PathVariable(value = "orderBy")String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if (pageSize == null){
+            pageNum = 10;
+        }
+        if (StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+
+        return iProductService.getProductByKeywordCategory("",categoryId, pageNum, pageSize, orderBy);
+    }
+
+    //Ambiguous handler methods mapped for HTTP path 'http://www.happymall.com/product/100012/1/10/price_asc'
+    @RequestMapping("/{keyword}/{pageNum}/{pageSize}/{orderBy}")
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTfulBadcase(@PathVariable(value = "keyword")String keyword,
+                                                @PathVariable(value = "pageNum")Integer pageNum,
+                                                @PathVariable(value = "pageSize")Integer pageSize,
+                                                @PathVariable(value = "orderBy")String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if (pageSize == null){
+            pageNum = 10;
+        }
+        if (StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+
+        return iProductService.getProductByKeywordCategory(keyword,null, pageNum, pageSize, orderBy);
+    }
+
+    //http://www.happymall.com/product/keyword/手机/1/10/price_asc
+    @RequestMapping("/keyword/{keyword}/{pageNum}/{pageSize}/{orderBy}")
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful(@PathVariable(value = "keyword")String keyword,
+                                                       @PathVariable(value = "pageNum")Integer pageNum,
+                                                       @PathVariable(value = "pageSize")Integer pageSize,
+                                                       @PathVariable(value = "orderBy")String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if (pageSize == null){
+            pageNum = 10;
+        }
+        if (StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+
+        return iProductService.getProductByKeywordCategory(keyword,null, pageNum, pageSize, orderBy);
+    }
+
+    //http://www.happymall.com/product/category/100012/1/10/price_asc
+    @RequestMapping("/category/{categoryId}/{pageNum}/{pageSize}/{orderBy}")
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful(@PathVariable(value = "categoryId")Integer categoryId,
+                                                       @PathVariable(value = "pageNum")Integer pageNum,
+                                                       @PathVariable(value = "pageSize")Integer pageSize,
+                                                       @PathVariable(value = "orderBy")String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if (pageSize == null){
+            pageNum = 10;
+        }
+        if (StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+
+        return iProductService.getProductByKeywordCategory("",categoryId, pageNum, pageSize, orderBy);
+    }
 
 }
