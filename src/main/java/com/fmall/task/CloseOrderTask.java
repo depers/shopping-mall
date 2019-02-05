121285c331f4999a5f65b71630a29d9f45fec13e
depers <dev_fengxiao@163.com>
2019-02-05 21:54:24 +0800

    finish

--- src/main/java/com/fmall/task/CloseOrderTask.java
+++ src/main/java/com/fmall/task/CloseOrderTask.java
@@ -66,7 +66,7 @@ public class CloseOrderTask {
     }
 
 
-    //@Scheduled(cron = "0 */1 * * * ?")
+    @Scheduled(cron = "0 */1 * * * ?")
     public void closeOrderTaskV3(){
         log.info("关闭订单定时任务启动");
 
@@ -98,7 +98,7 @@ public class CloseOrderTask {
         log.info("关闭订单定时任务结束");
     }
 
-    @Scheduled(cron = "0 */1 * * * ?")
+    //@Scheduled(cron = "0 */1 * * * ?")
     public void closeOrderTaskV4(){
         RLock lock = redissonManager.getRedisson().getLock(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
         boolean getLock = false;
